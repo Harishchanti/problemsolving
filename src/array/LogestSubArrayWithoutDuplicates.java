79a6221507e9a17ac98c9c7c61f562ce0f850a2b
h0v00n6 <Harish.Venkatesha@walmart.com>
2026-02-20 20:54:46 +0530

    longest possible sub array

---
+++ src/array/LogestSubArrayWithoutDuplicates.java
@@ -0,0 +1,34 @@
+package array;
+
+public class LogestSubArrayWithoutDuplicates {
+    public static void main(String[] args) {
+
+        String input = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(input));
+
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        if (s.length() <= 1)
+            return s.length();
+
+        boolean[] visted = new boolean[128];
+        int r = 0, l = 0, res = 0;
+
+        while (r < s.length()) {
+
+            while (visted[s.charAt(r)] == true) {
+                visted[s.charAt(l)] = false;
+                l++;
+            }
+
+            visted[s.charAt(r)] = true;
+
+            res = Math.max(res, r - l + 1);
+            r++;
+
+        }
+        return res;
+    }
+}
