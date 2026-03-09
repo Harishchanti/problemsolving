83c90ee664f1012ea6f41f403881f4b91ccef7d9
h0v00n6 <Harish.Venkatesha@walmart.com>
2026-03-09 18:25:21 +0530

    adding word ladder

--- src/array/TappingRainWater.java
+++ src/array/TappingRainWater.java
@@ -16,9 +16,36 @@ public class TappingRainWater {
         int[] height1 = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }; // 6   // {0,0,1,0,1,2,1,0,0,1}
 
 
+        //bruteforce approach (o) n2
         System.out.println(trapedWater(height));
-        System.out.println(trapedWater(height1));
+
+        //better approach approach (O) n space (O) n
+        System.out.println(trapedWaterV2(height1));
         //
+    }
+
+    private static int trapedWaterV2(int[] height1) {
+        int l = height1.length;
+        int[] leftMax = new int[l];
+        int[] rightMax = new int[l];
+
+        int max = height1[0];
+        for(int i = 0; i < height1.length ; i++) {
+            max =Math.max(max,height1[i]);
+            leftMax[i] = max;
+        }
+
+         max = height1[l-1];
+        for(int i = l-1; i >=0 ; i--) {
+            max = Math.max(max,height1[i]);
+            rightMax[i] = max;
+        }
+        int result = 0;
+        for (int i=0;i < l;i++) {
+            result += Math.min(leftMax[i],rightMax[i]) - height1[i];
+        }
+        return result;
+
     }
 
     private static int trapedWater(int[] height) {
---
+++ src/backtracking/WordLadder.java
@@ -0,0 +1,10 @@
+package backtracking;
+
+/*
+https://www.geeksforgeeks.org/dsa/word-ladder-length-of-shortest-chain-to-reach-a-target-word/
+ */
+public class WordLadder {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
