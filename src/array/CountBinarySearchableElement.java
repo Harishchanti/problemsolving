24f1c4e6e8162dcbc80df6f751dd589d89167c65
h0v00n6 <Harish.Venkatesha@walmart.com>
2026-03-26 14:40:11 +0530

    binary search problem

--- src/array/CountBinarySearchableElement.java
+++ src/array/CountBinarySearchableElement.java
@@ -17,7 +17,7 @@ public class CountBinarySearchableElement {
 
         // If these conditions follow that means Arr[m] is
         // binary searchable.
-        if (LR < Arr[m] && Arr[m] < RL)
+        if (LR <= Arr[m] && Arr[m] <= RL)
             ans = 1;
 
         // Finding the binary searchable elements to the
@@ -35,12 +35,44 @@ public class CountBinarySearchableElement {
 
     public static void main(String[] args) {
         //int[] Arr = { 10, 1, 2, 3, 4, 8, 6, 5, 7, 12, 9, 8, 13, 15, 11 };
-        int[] Arr = {3, 3, 2};
+        int[] Arr = { 3, 3, 2 };
         int n = Arr.length;
 
+        // bruteforce approach
+        System.out.println("Count : " + countBinarySearchableV1(Arr));
+
+        // optimizes approach.
         System.out.print(
                 "Number of Binary Searchable Indexes: ");
         System.out.println(countBinarySearchableIndex(
-                Arr, 0, n - 1, Integer.MIN_VALUE, Integer.MAX_VALUE ));
+                Arr, 0, n - 1, Integer.MIN_VALUE, Integer.MAX_VALUE));
+    }
+
+    private static int countBinarySearchableV1(int[] arr) {
+        int count = 0;
+        for (int a : arr) {
+            if (found(a, arr)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean found(int a, int[] arr) {
+        int i = 0;
+        int j = arr.length;
+
+        while (i < j) {
+            int mid = (i + j) / 2;
+
+            if (arr[mid] == a)
+                return true;
+            else if (a > arr[mid]) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return false;
     }
 }
